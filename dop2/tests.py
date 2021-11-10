b37dc60f31d646f3b9aaddf40d0045298ed9a16a
chintan-jagad <chj1409@gmail.com>
2021-11-10 16:41:55 +0530

    Made some changes

--- dop2/tests.py
+++ dop2/tests.py
@@ -7,4 +7,4 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
             request = HttpRequest()  #1
             response = home(request)  #2
-            self.assertEqual(b'Hello World!', response.content)  #4
+            self.assertEqual(b'Hello World', response.content)  #4
