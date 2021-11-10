047f046e8206745c3c2efe3a385e1984bd6dd77f
chintan-jagad <chj1409@gmail.com>
2021-11-10 16:37:29 +0530

    Added Tests

---
+++ dop2/tests.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+from django.http import HttpRequest
+from .views import home
+
+class HomePageTest(TestCase):
+
+    def test_home_page_returns_correct_html(self):
+            request = HttpRequest()  #1
+            response = home(request)  #2
+            self.assertEqual(b'Hello World', response.content)  #4
