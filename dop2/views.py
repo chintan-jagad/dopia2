4efe6fd0348f53871f73271ed98c7ea02045e9dc
ishajoglekar <ishasjoglekar@gmail.com>
2021-11-17 11:35:44 +0530

    updated

--- dop2/views.py
+++ dop2/views.py
@@ -1,4 +1,4 @@
-from django.http.response import HttpResponse
+from    django.http.response import HttpResponse
 
 def home(request):
-    return HttpResponse('Hello World!')
+    return HttpResponse('Hello World! Devops IA2')
